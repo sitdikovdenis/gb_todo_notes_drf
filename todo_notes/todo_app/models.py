155a27aaf32cf283ef201bd53c876532ce769e8f
sitdikov_denis <fomk@mail.ru>
2021-03-12 00:29:07 +0300

    Сделал модели в 3 задании

---
+++ todo_notes/todo_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from uuid import uuid4
+from library.models import Author
+
+
+class Project(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=1024)
+    repository_url = models.URLField()
+    users = models.ManyToManyField(Author)
+
+
+class TODO(models.Model):
+    CHOICES = (('C', 'Closed'),
+               ('A', 'Active'))
+
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='projects')
+    text = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
+    state = models.CharField(choices=CHOICES, max_length=7)
--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'library',
     'corsheaders',
+    'todo_app',
 ]
 
 MIDDLEWARE = [
