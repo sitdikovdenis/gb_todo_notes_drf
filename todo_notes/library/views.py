0bb26f7aeaf91b2a26d3d9dc064636d60e4f9ba9
sitdikov_denis <fomk@mail.ru>
2021-03-14 23:36:27 +0300

    модель User: есть возможность просмотра списка и каждого пользователя в отдельности, можно вносить изменения, нельзя удалять и создавать;

--- todo_notes/library/views.py
+++ todo_notes/library/views.py
@@ -1,9 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
+# from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins, viewsets
 
 from .models import Author
 from .serializers import AuthorModelSerializer
 
 
-class AuthorModelViewSet(ModelViewSet):
+class AuthorModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
